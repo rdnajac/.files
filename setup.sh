6631316c628cbc562a5704dcfda79d9c096c9a8a
Ryan D. Najac <ryan.najac@columbia.edu>
2024-10-21 13:07:23 -0400

    make setup posix compliant

--- setup.sh
+++ setup.sh
@@ -1,36 +1,43 @@
-#!/bin/bash
-REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && git rev-parse --show-toplevel)"
-echo "$REPO_ROOT"
+#!/usr/bin/env sh
+
+# Get the root of the repository
+REPO_ROOT="$(cd "$(dirname "$0")" && git rev-parse --show-toplevel)"
+echo "Repository Root: $REPO_ROOT"
 
 # Create a symlink in the home directory
 # Arguments: $1 - dotfile name (e.g. tmux.conf)
 make_symlink() {
-	local dest="${HOME}/.${1}"
+	if [ "$1" = "" ]; then
+		echo "Error: No filename provided."
+		return 1
+	fi
+
+	dest="${HOME}/.$1"
 	if [ -e "$dest" ]; then
-		echo "found .$1 in home directory"
+		echo "Found .$1 in home directory."
 		if [ ! -L "$dest" ]; then
-			echo "moving .$1 to .$1.old"
+			echo "Moving .$1 to .$1.old"
 			mv -iv "$dest" "$dest.old"
 		fi
 	fi
-	echo "linking $1..."
+	echo "Linking $1..."
 	ln -sfv "$REPO_ROOT/$1" "$dest"
 }
 
-make_symlink bash_aliases
-make_symlink tmux.conf
-make_symlink alacritty.toml
+make_symlink "alacritty.toml"
+make_symlink "bash_aliases"
+make_symlink "gitconfig"
+make_symlink "tmux.conf"
 
-# Prompt to clone vim config
-read -p "Do you want to clone vim config? [y/n] " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	# (cd ~ && git clone --recurse-submodules)
+# Symlink zshrc if on macOS
+if [ "$(uname)" = "Darwin" ]; then
+	make_symlink "zshrc"
 fi
 
-# [[ command zsh > /dev/null ]] && make_symlink zshrc
-if [ "$(uname)" = "Darwin" ]; then
-	make_symlink zshrc
+if prompt_user "Do you want to clone vim config?"; then
+	# Uncomment and specify the desired git repository
+	# git clone --recurse-submodules <vim_repo_url> ~/.vim
+	echo "Cloning vim config..."
 fi
 
 exec "$SHELL"
