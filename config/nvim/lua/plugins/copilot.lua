ff56bad9d490b6ef5a662e6c99076a57c24e3a3d
Ryan D. Najac <ryan.najac@columbia.edu>
2025-02-27 09:17:27 -0500

    tab for copilot

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -2,13 +2,13 @@ return {
   {
     'github/copilot.vim',
     enabled = true,
-    init = function()
-      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false,
-      })
-      vim.g.copilot_no_tab_map = true
-    end,
+    -- init = function()
+    --   vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+    --     expr = true,
+    --     replace_keycodes = false,
+    --   })
+    --   vim.g.copilot_no_tab_map = true
+    -- end,
   },
 
   {
