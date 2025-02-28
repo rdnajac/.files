e932a971ead8f7ea7814839e846d295952c9b084
Ryan D. Najac <ryan.najac@columbia.edu>
2025-02-28 13:53:36 -0500

    rename

---
+++ config/nvim/lua/plugins/binf.lua
@@ -0,0 +1,16 @@
+return {
+  { 'LukeGoodsell/nextflow-vim' },
+  {
+    'R-nvim/R.nvim',
+    opts = {
+      -- R_args = { '--quiet', '--no-save' },
+      R_app = '/Users/rdn/.local/bin/sshR',
+      R_cmd = '/Users/rdn/.local/bin/sshR',
+      compldir = '/Users/rdn/.remoteR',
+      remote_compldir = '/home/ubuntu/.cache/R.nvim',
+      local_R_library_dir = '/Users/rdn/tmp',
+      -- local_R_library_dir = '/Users/rdn/.local/share/nvim/lazy/R.nvim',
+      -- local_R_library_dir = '/home/ubuntu/tmp/nvimcom'
+    },
+  },
+}
