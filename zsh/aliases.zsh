8df84d9f18aafad60cf317ccb6257d2929f04e37
Ryan D. Najac <ryan.najac@columbia.edu>
2025-02-28 17:46:42 -0500

    update snippets

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -22,3 +22,5 @@ alias   nn='nvim ${DOTDIR}/etc/nnn.sh'
 # better versions of things in rust
 alias cat=bat
 alias l='eza --all --tree -l -L3 --group-directories-first --colour=always --icons=auto --git-ignore'
+alias ls='eza --group-directories-first --colour=always --icons=auto'
+alias ll='eza --all -l --group-directories-first --colour=always --icons=auto'
