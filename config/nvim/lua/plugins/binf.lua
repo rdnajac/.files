257985abf701936f0b255051ff0a3f194cf6e9e1
Ryan D. Najac <ryan.najac@columbia.edu>
2025-03-07 09:54:53 -0500

    add kickstarter plugins for quatro

--- config/nvim/lua/plugins/binf.lua
+++ config/nvim/lua/plugins/binf.lua
@@ -1,7 +1,21 @@
 return {
   { 'LukeGoodsell/nextflow-vim' },
   {
+    'R-nvim/R.nvim',
+    opts = {
+      R_args = { '--quiet' },
+      -- R_args = { '--quiet', '--no-save' },
+      -- R_app = '/Users/rdn/.local/bin/sshR',
+      -- R_cmd = '/Users/rdn/.local/bin/sshR',
+      -- compldir = '/Users/rdn/.remoteR',
+      -- remote_compldir = '/home/ubuntu/.cache/R.nvim',
+      -- local_R_library_dir = '/Users/rdn/.local/share/nvim/lazy/R.nvim',
+    },
+  },
+
+  {
     'quarto-dev/quarto-nvim',
+    ft = { 'quarto' },
     dependencies = {
       'jmbuhr/otter.nvim',
     },
@@ -9,15 +23,133 @@ return {
       -- { '<leader>cp', quarto.quartoPreview, { silent = true, noremap = true } },
     },
   },
-  {
-    'R-nvim/R.nvim',
+
+  { -- directly open ipynb files as quarto docuements and convert back behind the scenes
+    'GCBallesteros/jupytext.nvim',
+    enabled = false,
     opts = {
-      -- R_args = { '--quiet', '--no-save' },
-      -- R_app = '/Users/rdn/.local/bin/sshR',
-      -- R_cmd = '/Users/rdn/.local/bin/sshR',
-      -- compldir = '/Users/rdn/.remoteR',
-      -- remote_compldir = '/home/ubuntu/.cache/R.nvim',
-      -- local_R_library_dir = '/Users/rdn/.local/share/nvim/lazy/R.nvim',
+      custom_language_formatting = {
+        python = {
+          extension = 'qmd',
+          style = 'quarto',
+          force_ft = 'quarto',
+        },
+        r = {
+          extension = 'qmd',
+          style = 'quarto',
+          force_ft = 'quarto',
+        },
+      },
+    },
+  },
+
+  { -- send code from python/r/qmd documets to a terminal or REPL
+    -- like ipython, R, bash
+    'jpalardy/vim-slime',
+    enabled = false,
+    init = function()
+      vim.b['quarto_is_python_chunk'] = false
+      Quarto_is_in_python_chunk = function()
+        require('otter.tools.functions').is_otter_language_context('python')
+      end
+
+      vim.cmd([[
+      let g:slime_dispatch_ipython_pause = 100
+      function SlimeOverride_EscapeText_quarto(text)
+      call v:lua.Quarto_is_in_python_chunk()
+      if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk && !(exists('b:quarto_is_r_mode') && b:quarto_is_r_mode)
+      return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+      else
+      if exists('b:quarto_is_r_mode') && b:quarto_is_r_mode && b:quarto_is_python_chunk
+      return [a:text, "\n"]
+      else
+      return [a:text]
+      end
+      end
+      endfunction
+      ]])
+
+      vim.g.slime_target = 'neovim'
+      vim.g.slime_no_mappings = true
+      vim.g.slime_python_ipython = 1
+    end,
+    config = function()
+      vim.g.slime_input_pid = false
+      vim.g.slime_suggest_default = true
+      vim.g.slime_menu_config = false
+      vim.g.slime_neovim_ignore_unlisted = true
+
+      local function mark_terminal()
+        local job_id = vim.b.terminal_job_id
+        vim.print('job_id: ' .. job_id)
+      end
+
+      local function set_terminal()
+        vim.fn.call('slime#config', {})
+      end
+      vim.keymap.set('n', '<leader>cm', mark_terminal, { desc = '[m]ark terminal' })
+      vim.keymap.set('n', '<leader>cs', set_terminal, { desc = '[s]et terminal' })
+    end,
+  },
+
+  { -- paste an image from the clipboard or drag-and-drop
+    'HakonHarnes/img-clip.nvim',
+    enabled = false,
+    event = 'BufEnter',
+    ft = { 'markdown', 'quarto', 'latex' },
+    opts = {
+      default = {
+        dir_path = 'img',
+      },
+      filetypes = {
+        markdown = {
+          url_encode_path = true,
+          template = '![$CURSOR]($FILE_PATH)',
+          drag_and_drop = {
+            download_images = false,
+          },
+        },
+        quarto = {
+          url_encode_path = true,
+          template = '![$CURSOR]($FILE_PATH)',
+          drag_and_drop = {
+            download_images = false,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require('img-clip').setup(opts)
+      vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
+    end,
+  },
+
+  { -- preview equations
+    'jbyuki/nabla.nvim',
+    enabled = false,
+    keys = {
+      { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
+    },
+  },
+
+  {
+    'benlubas/molten-nvim',
+    enabled = false,
+    build = ':UpdateRemotePlugins',
+    init = function()
+      vim.g.molten_image_provider = 'image.nvim'
+      vim.g.molten_output_win_max_height = 20
+      vim.g.molten_auto_open_output = false
+    end,
+    keys = {
+      { '<leader>mi', ':MoltenInit<cr>', desc = '[m]olten [i]nit' },
+      {
+        '<leader>mv',
+        ':<C-u>MoltenEvaluateVisual<cr>',
+        mode = 'v',
+        desc = 'molten eval visual',
+      },
+      { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
     },
   },
 }
--- home/.Rprofile
+++ home/.Rprofile
@@ -7,8 +7,17 @@
 ## │ we can configure this with nvim                                │
 ## ╰────────────────────────────────────────────────────────────────╯
 
+options(repos = c(CRAN = "https://cran.mirrors.hoobly.com/"))
+
 if (!require("BiocManager", quietly = TRUE)) {
   install.packages("BiocManager")
 }
+
+# Install packages with the following sytax:
+# `BiocManager::install("package")`
+# Youc can also install Packages from Bioconductor 
+# wiht renv by using the bioc:: prefix. For example:
+# `renv::install("bioc::Biobase")`
+
 # options(nvimcom.verbose = 2)
 # library(colorout)
