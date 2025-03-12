63d44ce3538504b65ea4282572d92554c0ec9f29
Ryan D. Najac <ryan.najac@columbia.edu>
2025-03-12 16:54:55 -0400

    clean up blink config

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -29,4 +29,88 @@ return {
       })
     end,
   },
+
+  ---@module 'blink.cmp'
+  {
+    'Saghen/blink.cmp',
+    dependencies = { { 'rafamadriz/friendly-snippets', enabled = false } },
+    ---@type blink.cmp.Config
+    opts = {
+      completion = {
+        accept = { auto_brackets = { enabled = false } },
+        documentation = { window = { border = 'single' } },
+        menu = {
+          auto_show = false,
+          border = 'rounded',
+        },
+      },
+      signature = {
+        enabled = false,
+        window = { border = 'rounded' },
+      },
+
+      keymap = {
+        preset = 'super-tab',
+        ['J'] = {
+          function(cmp)
+            if cmp.is_menu_visible() then
+              return cmp.select_next()
+            end
+          end,
+          'fallback',
+        },
+        ['K'] = {
+          function(cmp)
+            if cmp.is_menu_visible() then
+              return cmp.select_prev()
+            end
+          end,
+          'fallback',
+        },
+      },
+
+      cmdline = {
+        enabled = false,
+        sources = {},
+      },
+
+      sources = {
+        compat = {},
+        default = { 'lazydev', 'path', 'lsp', 'snippets' },
+
+        providers = {
+          path = {
+            name = 'Path',
+            module = 'blink.cmp.sources.path',
+            score_offset = 333,
+            fallbacks = { 'buffer' },
+            opts = {
+              trailing_slash = true,
+              label_trailing_slash = true,
+              get_cwd = function(_)
+                return vim.fn.getcwd()
+              end,
+              show_hidden_files_by_default = true,
+            },
+          },
+          snippets = {
+            name = 'Snippets',
+            module = 'blink.cmp.sources.snippets',
+            opts = {
+              friendly_snippets = false,
+              search_paths = { vim.fn.stdpath('config') .. '/snippets' },
+              global_snippets = { 'all' },
+              extended_filetypes = {},
+              ignored_filetypes = {},
+              get_filetype = function(_)
+                return vim.bo.filetype
+              end,
+              -- Set to '+' to use the system clipboard, or '"' to use the unnamed register
+              clipboard_register = nil,
+            },
+          },
+        },
+      },
+    },
+  },
 }
