cad3881ad17db3789b40e64e81bab8b4d9b5bebe
Ryan D. Najac <ryan.najac@columbia.edu>
2025-03-31 03:11:16 -0400

    use nerdfont icons

--- config/tmux/bin/tmux-battery.sh
+++ config/tmux/bin/tmux-battery.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Get the battery percentage as a number (remove the % sign)
 battery_percent=$(pmset -g batt | grep -o "[0-9]\{1,3\}%" | tr -d '%')
@@ -6,8 +6,9 @@ battery_percent=$(pmset -g batt | grep -o "[0-9]\{1,3\}%" | tr -d '%')
 # Get the charging status
 charging_status=$(pmset -g batt | grep "AC Power")
 
-# Determine the icon based on the battery percentage
-if [ "$battery_percent" -eq 100 ]; then
+if [ -n "$charging_status" ]; then
+    icon=" "
+elif [ "$battery_percent" -eq 100 ]; then
     icon="🔌"
 elif [ "$battery_percent" -le 20 ]; then
     icon="🪫"
@@ -15,12 +16,5 @@ else
     icon="🔋"
 fi
 
-# Add the lightning bolt if charging
-if [ -n "$charging_status" ]; then
-    bolt="⚡️"
-else
-    bolt=""
-fi
-
 # Print the icon, battery percentage, and optional lightning bolt
-printf "%s %d%% %s\n" "$icon" "$battery_percent" "$bolt"
+printf "%s%d%%\n" "$icon" "$battery_percent"
