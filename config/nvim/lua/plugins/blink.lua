9ba424f26a44c0357e4e3e9f8f3ea17dbe4d1b43
Ryan D. Najac <ryan.najac@columbia.edu>
2025-04-07 03:16:56 -0400

    update blink

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -18,36 +18,19 @@ return {
     opts.keymap = {
       preset = 'super-tab',
     }
-    -- opts.sources = {
-    --   providers = {
-    --     path = {
-    --       name = 'Path',
-    --       module = 'blink.cmp.sources.path',
-    --       show_hidden_files_by_default = true,
-    --     },
-    --   },
-    -- }
-
-    -- opts.sources.providers.path.show_hidden_files_by_default = true
+    opts.sources = {
+      providers = {
+        lsp = {
+          name = 'LSP',
+          module = 'blink.cmp.sources.lsp',
+          transform_items = function(_, items)
+            return vim.tbl_filter(function(item)
+              return item.kind ~= require('blink.cmp.types').CompletionItemKind.Keyword
+            end, items)
+          end,
+        },
+        path = { opts = { show_hidden_files_by_default = true } },
+      },
+    }
   end,
 }
--- ~/.
---     sources = {
---       transform_items = function(_, items)
---         return vim.tbl_filter(function(item)
---           return item.kind ~= require('blink.cmp.types').CompletionItemKind.Snippet
---         end, items)
---       end,
-
---       providers = {
---         path = {
---           score_offset = 333,
---           fallbacks = { 'buffer' },
---           opts = {
---           },
---         },
---       },
---     },
---   }
---   return config
--- }
