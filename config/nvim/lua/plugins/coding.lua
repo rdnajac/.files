80d8ef18bcf51274bd7c208940f54ae82e9e161d
Ryan D. Najac <ryan.najac@columbia.edu>
2025-04-07 03:16:43 -0400

    add ai

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -24,38 +24,4 @@ return {
     end,
   },
 
-  -- https://codecompanion.olimorris.dev
-  {
-    'olimorris/codecompanion.nvim',
-    enabled = false,
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    opts = {
-      strategies = {
-        -- Change copilot default chat adapter
-        chat = {
-          adapter = 'copilot',
-        },
-      },
-      opts = {
-        -- Set debug logging
-        log_level = 'DEBUG',
-      },
-    },
-  },
-
-  {
-    'github/copilot.vim',
-    endabled = true,
-    event = 'VeryLazy',
-    init = function()
-      vim.cmd([[
-        imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
-        let g:copilot_no_tab_map = v:true
-        let g:copilot_workspace_folders = ["~/GitHub"]
-      ]])
-    end,
-  },
 }
