f7c2bd7677eb26bbb18ce9b327df8941e4fa08ce
Ryan D. Najac <ryan.najac@columbia.edu>
2025-05-14 14:10:24 -0400

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,35 @@
-#!/usr/bin/env sh
-## Symlink all dotfiles
-set -eux
+#!/bin/bash
+#
+# Symlink all dotfiles and `XDG_CONFIG_DIR`s
+set -eu
+IFS=$'\n\t'
 
-THISDIR=$(dirname "$(realpath "$0")")
+die() {
+	printf 'fatal: %s\n' "$1" >&2
+	exit 1
+}
 
-ln -sfnv "${THISDIR}/home/.Rprofile" "${HOME}/.Rprofile"
-ln -sfnv "${THISDIR}/home/.bash_aliases" "${HOME}/.bash_aliases"
+link() {
+	[[ $# -ne 2 ]] && die "usage: link <src> <dst>"
+	local src=$1 dst=$2
+	[[ ! -e $src ]] && die "$src does not exist"
+	ln -sfnv "$src" "$dst"
+}
 
+readonly THISDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+readonly XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
-if [ ! -d "${XDG_CONFIG_HOME:-$HOME/.config}" ]; then
-	mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/"
-fi
+mkdir -p "$XDG_CONFIG_HOME"
 
-for confdir in ./config/*; do
-	ln -sfnv "$(realpath "$confdir")" "${XDG_CONFIG_HOME:-$HOME/.config}/$(basename "$confdir")"
+link "$THISDIR/home/.Rprofile" "$HOME/.Rprofile"
+link "$THISDIR/home/.bash_aliases" "$HOME/.bash_aliases"
+
+for src in "$THISDIR"/config/*; do
+	[[ -d $src ]] || continue
+	link "$src" "$XDG_CONFIG_HOME/$(basename "$src")"
 done
 
-# IMPORTANT! use double quotes around EOF so that $THISDIR expands
+# IMPORTANT! $ will expand only if EOF is unquoted or double-quoted
 # IMPORTANT! prepend the `$` with a `\` so it does not expand
 cat << EOF > "$HOME/.zshenv"
 export DOTDIR=$THISDIR
--- etc/neovim.sh
+++ etc/neovim.sh
@@ -2,6 +2,7 @@ export MANPAGER="nvim +Man!"
 
 export NVIM_SOCK="${XDG_RUNTIME_DIR:-$HOME/.cache}/nvim/server.pipe"
 alias nv=e
+alias lv='NVIM_APPNAME=lazyvim nvim'
 
 alias zr='nvim ${ZDOTDIR}'
 alias zrc='nvim ${ZDOTDIR}/.zshrc'
