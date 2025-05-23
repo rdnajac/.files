128216b034c37c4671b4bd3e8627cb55730cc8a5
Ryan D. Najac <ryan.najac@columbia.edu>
2025-05-23 15:02:15 -0400

    Update .config/vim/lua/lazy/spec/mini.lua

--- dot_config/vim/lua/lazy/spec/mini.lua
+++ dot_config/vim/lua/lazy/spec/mini.lua
@@ -1,7 +1,14 @@
 return {
   'echasnovski/mini.nvim',
   event = 'VeryLazy',
+  init = function()
+    package.preload['nvim-web-devicons'] = function()
+      require('mini.icons').mock_nvim_web_devicons()
+      return package.loaded['nvim-web-devicons']
+    end
+  end,
   config = function()
     require('mini.align').setup()
+    require('mini.icons').setup()
   end,
 }
