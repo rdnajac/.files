b81af7fccec37b3b2400b40d59751d084a4f219c
Ryan D. Najac <ryan.najac@columbia.edu>
2025-05-27 10:53:30 -0400

    update chezmoi

--- dot_config/vim/lua/lazy/spec/chezmoi.lua
+++ dot_config/vim/lua/lazy/spec/chezmoi.lua
@@ -1,38 +1,3 @@
--- -- TODO make this a picker
--- local pick_chezmoi = function()
---   local results = require('chezmoi.commands').list({
---     args = {
---       '--path-style',
---       'absolute',
---       '--include',
---       'files',
---       '--exclude',
---       'externals',
---     },
---   })
---   local items = {}
---
---   for _, czFile in ipairs(results) do
---     table.insert(items, {
---       text = czFile,
---       file = czFile,
---     })
---   end
---
---   ---@type snacks.picker.Config
---   local opts = {
---     items = items,
---     confirm = function(picker, item)
---       picker:close()
---       require('chezmoi.commands').edit({
---         targets = { item.text },
---         args = { '--watch' },
---       })
---     end,
---   }
---   Snacks.picker.pick(opts)
--- end
---
 return {
   {
     -- highlighting for chezmoi files template files
@@ -58,9 +23,9 @@ return {
         on_apply = true,
         on_watch = false,
       },
-      telescope = {
-        select = { '<CR>' },
-      },
+      -- telescope = {
+      --   select = { '<CR>' },
+      -- },
     },
     init = function()
       -- run chezmoi edit on file enter
@@ -73,11 +38,11 @@ return {
     end,
   },
 
-  -- Filetype icons
   {
     'echasnovski/mini.icons',
     opts = {
       file = {
+        ['dot_zshrc'] = { glyph = '', hl = 'MiniIconsGreen' },
         ['.chezmoiignore'] = { glyph = '', hl = 'MiniIconsGrey' },
         ['.chezmoiremove'] = { glyph = '', hl = 'MiniIconsGrey' },
         ['.chezmoiroot'] = { glyph = '', hl = 'MiniIconsGrey' },
