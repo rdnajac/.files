1aa7184939f837e16534cd0cd0efc44c29f322b8
Ryan D. Najac <ryan.najac@columbia.edu>
2025-05-28 13:57:06 -0400

    update install scripts

--- .chezmoiscripts/run_once_install-rust.sh
+++ .chezmoiscripts/run_once_install-rust.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+## Install rust using rustup if it is not already installed.
 if ! command -v cargo > /dev/null; then
   echo "Installing rustup..."
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
