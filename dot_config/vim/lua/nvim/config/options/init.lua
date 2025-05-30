83a1666b1cd3dab2c72378dcdbcb5609295e9f10
Ryan D. Najac <ryan.najac@columbia.edu>
2025-05-30 15:25:22 -0400

    Update .config/vim/lua/nvim/config/options/init.lua Update .config/vim/lua/nvim/init.lua Update .config/vim/vimrc

--- dot_config/vim/lua/nvim/config/options/init.lua
+++ dot_config/vim/lua/nvim/config/options/init.lua
@@ -5,7 +5,7 @@ vim.opt.backup = true
 vim.opt.backupdir = vim.fn.stdpath('state') .. '/backup//'
 vim.opt.mousescroll = 'hor:0'
 vim.opt.pumblend = 0
-vim.opt.signcolumn = 'yes'
+-- vim.opt.signcolumn = 'yes'
 vim.opt.winborder = 'rounded'
 
 if vim.fn.has('nvim-0.12') == 1 then
