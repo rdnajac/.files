1c36ebd8940ebe6f311b037064f184b8cf1b7004
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-03 04:13:42 -0400

    Update .config/vim/lua/nvim/lazy/spec/init.lua

--- dot_config/vim/lua/nvim/lazy/spec/init.lua
+++ dot_config/vim/lua/nvim/lazy/spec/init.lua
@@ -1,16 +1,18 @@
-vim.g.lazyvim_check_order = false
-
 -- require('lazyvim.config').init()
 require('lazyvim.util.plugin').lazy_file()
-
 
 return {
   {
     'LazyVim/LazyVim',
+    init = function()
+      vim.g.lazyvim_check_order = false
+    end,
+
+    -- stylua: ignore
     ---@class LazyVimOptions
     opts = {
       defaults = { autocmds = false, keymaps = false },
-      news = { lazyvim = false, neovim = false },
+      news     = { lazyvim  = false, neovim  = false },
     },
   },
   { 'nvim-lua/plenary.nvim', lazy = true },
