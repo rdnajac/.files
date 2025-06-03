987a5629c1fdae93fe8c274bb76ed5c33d1d453c
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-02 23:47:04 -0400

    Update bin/scripts/common.sh Update .config/vim/lua/nvim/init.lua Remove .config/vim/lua/nvim/lazy/bootstrap.lua Add .config/vim/lua/nvim/lazy/init.lua

--- bin/scripts/common.sh
+++ bin/scripts/common.sh
@@ -1,10 +1,4 @@
 # POSIX-compliant helper functions
-# TODO: keep global variables here?
-
-# ENV_VAR_NAME="RDN_ENV"
-
-DOTDIR="$HOME/.files"
-
 info() {
 	printf "\033[33m%s\033[0m\n" "$*" >&2
 }
@@ -13,24 +7,11 @@ error() {
 	printf "\033[31m%s\033[0m\n" "$*" >&2
 }
 
-shallow_clone() {
-	if [ $# -ne 1 ]; then
-		error "usage: shallow_clone <repo-url>"
-		return 1
-	fi
-	info "Cloning $1 with depth=1..."
-	git clone --depth 1 "$1"
-}
-
 cd_or_die() {
 	cd "$1" || {
 		error "cd: cannot enter directory: $1"
 		exit 1
 	}
-}
-
-ensure_tpm_path_exists() {
-	mkdir -p "$(tpm_path)"
 }
 
 fail_helper() {
