df8469b06ae0671a292111680c16d8c243be8dbf
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-03 01:26:31 -0400

    Update .config/vim/lua/nvim/lazy/spec/init.lua

--- dot_config/vim/lua/nvim/lazy/spec/init.lua
+++ dot_config/vim/lua/nvim/lazy/spec/init.lua
@@ -1,10 +1,5 @@
 vim.g.lazyvim_check_order = false
-local Event = require('lazy.core.handler.event')
-Event.mappings.LazyFile = {
-  id = 'LazyFile',
-  event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
-}
-Event.mappings['User LazyFile'] = Event.mappings.LazyFile
+require("lazyvim.config").init()
 
 return {
   { 'nvim-lua/plenary.nvim', lazy = true },
