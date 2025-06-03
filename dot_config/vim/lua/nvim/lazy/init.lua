75ab509dc4b110381a853ab24eeb0dd2ff2f8668
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-03 01:41:23 -0400

    Update .config/vim/lua/nvim/lazy/init.lua

--- dot_config/vim/lua/nvim/lazy/init.lua
+++ dot_config/vim/lua/nvim/lazy/init.lua
@@ -7,7 +7,7 @@ else
 end
 
 require('lazy').setup({
-  spec = { import = 'nvim.lazy.spec' , },
+  spec = { import = 'nvim.lazy.spec' },
   dev = { path = vim.fn.stdpath('config') .. '/pack/vimfect/start' },
   install = { colorscheme = { 'tokyonight' } },
   ui = {
