49a4216225afc8aa9a272f60ded8c25a10e76485
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-03 01:04:36 -0400

    Update .config/vim/lua/nvim/lazy/spec/init.lua

--- dot_config/vim/lua/nvim/lazy/spec/init.lua
+++ dot_config/vim/lua/nvim/lazy/spec/init.lua
@@ -1,3 +1,4 @@
+vim.g.lazyvim_check_order = false
 local Event = require('lazy.core.handler.event')
 Event.mappings.LazyFile = {
   id = 'LazyFile',
@@ -6,22 +7,5 @@ Event.mappings.LazyFile = {
 Event.mappings['User LazyFile'] = Event.mappings.LazyFile
 
 return {
-  { -- https://www.lazyvim.org/news
-    'LazyVim/LazyVim',
-    priority = 9999,
-    dev = true,
-    init = function()
-      vim.g.lazyvim_check_order = false
-      -- noop if we skip importing plugins
-      -- package.loaded['lazyvim.config.options'] = true
-    end,
-    -- stylua: ignore
-    keys = {
-      { 'gL', function() require('nvim.lazy.goto') end, { desc = 'Goto LazyVim module' }, },
-      { '<leader>l', '<Cmd>Lazy<CR>', desc = 'Lazy' },
-      { '<leader>L', '<Cmd>LazyExtras<CR>', desc = 'LazyExtras' },
-    },
-    { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
-  },
   { 'nvim-lua/plenary.nvim', lazy = true },
 }
