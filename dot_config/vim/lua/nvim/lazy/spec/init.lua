77ff79d0bc0b6ef2f6b57e456963cf11cc32f915
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-03 01:56:03 -0400

    Update .config/vim/lua/nvim/lazy/spec/init.lua

--- dot_config/vim/lua/nvim/lazy/spec/init.lua
+++ dot_config/vim/lua/nvim/lazy/spec/init.lua
@@ -1,21 +1,17 @@
 vim.g.lazyvim_check_order = false
 
----@class LazyVimOptions
-local opts = {
-  defaults = {
-    autocmds = true,  -- lazyvim.config.autocmds
-    keymaps = true,   -- lazyvim.config.keymaps
-    -- lazyvim.config.options can't be configured here since that's loaded before lazyvim setup
-    -- if you want to disable loading options, add `package.loaded["lazyvim.config.options"] = true` to the top of your init.lua
-  },
-  news = {
-    lazyvim = true,
-    neovim = true,
-  },
-}
-require('lazyvim.config').setup(opts)
 require('lazyvim.config').init()
 
 return {
+
+  {
+    'LazyVim/LazyVim',
+
+---@class LazyVimOptions
+opts = {
+  defaults = { autocmds = false, keymaps = false, },
+  news = { lazyvim = true, neovim = true, },
+},
+  }
   { 'nvim-lua/plenary.nvim', lazy = true },
 }
