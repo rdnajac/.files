623a52e669b4d8c39bf53f403ecedfcf1c47c8ec
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-02 20:01:55 -0400

    Update .config/vim/lua/nvim/config/options/lsp.lua

--- dot_config/vim/lua/nvim/config/options/lsp.lua
+++ dot_config/vim/lua/nvim/config/options/lsp.lua
@@ -8,7 +8,7 @@ vim.lsp.config('*', {
   on_attach = function(client, bufnr)
     local opts = { buffer = bufnr }
 
-    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
