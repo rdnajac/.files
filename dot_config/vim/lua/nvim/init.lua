90cf7d1ae9a5b7e9f8ff4e63e478f1f51c489839
Ryan D. Najac <ryan.najac@columbia.edu>
2025-06-03 04:03:02 -0400

    Update .config/vim/lua/nvim/init.lua Update .config/vim/lua/nvim/lazy/spec/init.lua

--- dot_config/vim/lua/nvim/init.lua
+++ dot_config/vim/lua/nvim/init.lua
@@ -1,2 +1,12 @@
 require('nvim.lazy')
 require('nvim.util')
+
+local group = vim.api.nvim_create_augroup('LazyVim', { clear = true })
+vim.api.nvim_create_autocmd('User', {
+  group = group,
+  pattern = 'VeryLazy',
+  callback = function()
+    require('nvim.config')
+  end,
+})
+
--- dot_config/vim/lua/nvim/lazy/spec/init.lua
+++ dot_config/vim/lua/nvim/lazy/spec/init.lua
@@ -3,15 +3,6 @@ vim.g.lazyvim_check_order = false
 -- require('lazyvim.config').init()
 require('lazyvim.util.plugin').lazy_file()
 
-local group = vim.api.nvim_create_augroup('LazyVim', { clear = true })
-vim.api.nvim_create_autocmd('User', {
-  group = group,
-  pattern = 'VeryLazy',
-  callback = function()
-    require('nvim.config')
-  end,
-})
-
 return {
   {
     'LazyVim/LazyVim',
