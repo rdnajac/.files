f2e2a9ec9aa6d7fbe2df50356766a19656f039c8
Ryan D. Najac <ryan.najac@columbia.edu>
2025-07-10 03:55:25 -0400

    aws s3 completion

--- dot_config/env/nvim.sh
+++ dot_config/env/nvim.sh
@@ -6,3 +6,14 @@ alias lv='NVIM_APPNAME=lazyvim nvim'
 alias repro='NVIM_APPNAME=repro nvim'
 alias zzz='nvim -c "lua Snacks.lazygit()"'
 alias nvr='nvim --headless --noplugin --server "$NVIM_SOCK" --remote-expr'
+
+oil() {
+	if [[ $1==*:* ]]; then
+		h=${1%%:*}
+		p=${1#*:}
+	else
+		h=$1
+		p=~
+	fi
+	nvim oil-ssh://"$h/$p"
+}
--- dot_config/zsh/aws.zsh
+++ dot_config/zsh/aws.zsh
@@ -1,50 +1,41 @@
-# AWS S3 caching and completion {{{
+# AWS S3 caching and completion
 
-# Cache directories
+# cache dirs
 ZSH_CACHE_DIR="${ZSH_CACHE_DIR:-$HOME/.cache/zsh}"
 S3_CACHE="$ZSH_CACHE_DIR/aws-s3"
 mkdir -p "$S3_CACHE"
 
-# Cache update function
+# manual refresh
 s3-refresh-cache() {
-	echo "Refreshing S3 cache..."
 	aws s3 ls | awk '{print $3}' > "$S3_CACHE/buckets" || return
 	while read -r bucket; do
-		echo "Caching keys for: $bucket"
 		aws s3 ls "s3://$bucket/" --recursive \
 			| awk '{$1=$2=$3=""; sub(/^ +/, ""); print}' \
 			> "$S3_CACHE/${bucket}-keys"
 	done < "$S3_CACHE/buckets"
-	echo "Done."
 }
 
-# Completion helper
+# completion helper
 _aws_s3_complete() {
-	local cur="${words[CURRENT]}"
-	if [[ "$cur" == s3://*/* ]]; then
-		local bucket="${cur#s3://}"; bucket="${bucket%%/*}"
-		local keyfile="$S3_CACHE/${bucket}-keys"
+	local cur bucket prefix keyfile keys
+	cur=$words[CURRENT]
+
+	if [[ $cur == s3://*/* ]]; then
+		bucket=${cur#s3://}; bucket=${bucket%%/*}
+		prefix=${cur#*"$bucket/"}
+		keyfile=$S3_CACHE/${bucket}-keys
 		[[ -r $keyfile ]] || return
-		compadd -W "s3://$bucket/" -- ${(f)"$(<"$keyfile")"}
-	elif [[ "$cur" == s3://* || -z "$cur" ]]; then
-		local buckets_file="$S3_CACHE/buckets"
-		[[ -r $buckets_file ]] || return
-		compadd -W "s3://" -- ${(f)"$(<"$buckets_file")"}
+		keys=(${(f)"$(<"$keyfile")"})
+		compadd -P "s3://$bucket/" -- ${(M)keys:#$prefix*}
+	elif [[ $cur == s3://* ]]; then
+		compadd -P 's3://' -S '/' ${(f)"$(<"$S3_CACHE/buckets")"}
 	fi
 }
 
-# Register for s3 aliases
-compdef _aws_s3_complete s3cp s3ls s3rm s3mv s3sync
+s3ls()   { aws s3 ls   "$@" }
+s3cp()   { aws s3 cp   "$@" }
+s3rm()   { aws s3 rm   "$@" }
+s3mv()   { aws s3 mv   "$@" }
+s3sync() { aws s3 sync "$@" }
 
-# fzf helper
-s3fzf() {
-	local bucket file
-	bucket=$(<"$S3_CACHE/buckets" fzf --prompt="Bucket: ") || return
-	file=$(<"$S3_CACHE/${bucket}-keys" fzf --prompt="File: ") || return
-	echo "s3://$bucket/$file"
-}
-
-# Optionally alias s3f to copy path to clipboard
-# s3f | pbcopy
-
-# }}}
+compdef _aws_s3_complete s3ls s3cp s3rm s3mv s3sync
