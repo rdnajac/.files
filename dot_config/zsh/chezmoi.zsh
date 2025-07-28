a712173236049bba34b576623e5aeb975eac1c3b
Ryan D. Najac <ryan.najac@columbia.edu>
2025-07-28 04:14:17 -0400

    fix nvim install

--- dot_config/zsh/chezmoi.zsh
+++ dot_config/zsh/chezmoi.zsh
@@ -13,6 +13,5 @@ alias czr='cz re-add'
 alias czs='cz status'
 alias czu='cz unmanaged'
 alias czx='cz execute-template'
-alias cztest=;cz execute-template --file;
-
+alias cztest='cz execute-template --file'
 alias czz='$VISUAL $CHEZMOI_SOURCE_DIR/dot_config/zsh'
