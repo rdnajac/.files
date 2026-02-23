c2ff260e3e3438d69756d1fd3f35383fe71c1e55
Ryan D. Najac <ryan.najac@columbia.edu>
2026-02-23 15:45:28 -0500

    fix fish

--- dot_config/private_fish/functions/copy.fish
+++ dot_config/private_fish/functions/copy.fish
@@ -1,12 +1,22 @@
 function copy
-  set file ${argv[1]:-/dev/stdin}
-  if command -v pbcopy &>/dev/null
-    pbcopy < "$file"
-  else if command -v xclip &>/dev/null
-    xclip -sel clip < "$file"
+  set -q argv[1]; and set file $argv[1]; or set file /dev/stdin
+
+  if type -q pbcopy
+    if test "$file" = "/dev/stdin"
+      pbcopy
+    else
+      cat "$file" | pbcopy
+    end
+  else if type -q xclip
+    if test "$file" = "/dev/stdin"
+      xclip -sel clip
+    else
+      cat "$file" | xclip -sel clip
+    end
   else
     printf "\033[0;31mNo clipboard utility found\033[0m\n"
     return 1
   end
+
   printf "\033[0;31mCopied contents of %s to clipboard\033[0m\n" "$file"
 end
--- dot_config/private_fish/functions/paste.fish
+++ dot_config/private_fish/functions/paste.fish
@@ -1,12 +1,22 @@
 function paste
-  set file ${argv[1]:-/dev/stdout}
-  if command -v pbpaste &>/dev/null
-    pbpaste > "$file"
-  else if command -v xclip &>/dev/null
-    xclip -sel clip -o > "$file"
+  set -q argv[1]; and set file $argv[1]; or set file /dev/stdout
+
+  if type -q pbpaste
+    if test "$file" = "/dev/stdout"
+      pbpaste
+    else
+      pbpaste | cat > "$file"
+    end
+  else if type -q xclip
+    if test "$file" = "/dev/stdout"
+      xclip -sel clip -o
+    else
+      xclip -sel clip -o | cat > "$file"
+    end
   else
     printf "\033[0;31mNo clipboard utility found\033[0m\n"
     return 1
   end
+
   printf "\033[0;31mPasted contents of clipboard to %s\033[0m\n" "$file"
 end
