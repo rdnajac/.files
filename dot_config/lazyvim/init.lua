80ae7299497784e0d058552db7a3495839a697dc
Ryan D. Najac <ryan.najac@columbia.edu>
2026-03-29 23:28:17 -0400

    update config

--- dot_config/lazyvim/init.lua
+++ dot_config/lazyvim/init.lua
@@ -1,55 +1,36 @@
 local lazypath = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy', 'lazy.nvim')
 if not vim.uv.fs_stat(lazypath) then
-  load(
-    vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua')
-  )()
+  assert(loadstring(vim.fn.system([[
+      curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua]]))())
 else
   vim.opt.rtp:prepend(lazypath)
 end
 
 local spec = {
-  { 'folke/lazy.nvim', version = false },
   {
     'LazyVim/LazyVim',
     version = false,
     opts = {
       defaults = {
         autocmds = true,
-        keymaps = false,
+        keymaps = true,
       },
     },
   },
+  { 'folke/lazy.nvim', version = false },
 }
 
 ---@type LazyConfig
 local opts = {
+  install = { colorscheme = { 'tokyonight' } },
   performance = {
     reset_packpath = false,
-    rtp = {
-      reset = false,
-      disabled_plugins = {
-        -- 'gzip',
-        -- 'matchit',
-        -- 'matchparen',
-        -- 'netrwPlugin',
-        -- 'tarPlugin',
-        -- 'tohtml',
-        -- 'tutor',
-        -- 'zipPlugin',
-      },
-    },
-  },
-  profiling = {
-    loader = false,
-    require = false,
+    rtp = { reset = false },
   },
   pkg = { enabled = false },
   rocks = { enabled = false },
-  -- install = { colorscheme = { 'tokyonight' } },
-  change_detection = {
-    enabled = true,
-    notify = true,
-  },
+  profiling = { loader = false, require = false },
+  change_detection = { enabled = false, notify = true },
 }
 
-require('lazy').setup(spec, opts)
+require('lazy').setup({ { 'LazyVim/LazyVim', import = 'lazyvim.plugins' }, spec }, opts)
